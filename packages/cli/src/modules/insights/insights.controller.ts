38b44e22dab381953d2aa58df11ffaf503c52013
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2025-04-14 17:21:12 +0200

    Check license permissions on insights controller routes

--- packages/cli/src/modules/insights/insights.controller.ts
+++ packages/cli/src/modules/insights/insights.controller.ts
@@ -2,6 +2,8 @@ import { ListInsightsWorkflowQueryDto } from '@n8n/api-types';
 import type { InsightsSummary, InsightsByTime, InsightsByWorkflow } from '@n8n/api-types';
 
 import { Get, GlobalScope, Query, RestController } from '@/decorators';
+import { ForbiddenError } from '@/errors/response-errors/forbidden.error';
+import { License } from '@/license';
 import { paginationListQueryMiddleware } from '@/middlewares/list-query/pagination';
 import { sortByQueryMiddleware } from '@/middlewares/list-query/sort-by';
 import { AuthenticatedRequest } from '@/requests';
@@ -12,7 +14,10 @@ import { InsightsService } from './insights.service';
 export class InsightsController {
 	private readonly maxAgeInDaysFilteredInsights = 7;
 
-	constructor(private readonly insightsService: InsightsService) {}
+	constructor(
+		private readonly insightsService: InsightsService,
+		private readonly licenseService: License,
+	) {}
 
 	@Get('/summary')
 	@GlobalScope('insights:list')
@@ -29,6 +34,10 @@ export class InsightsController {
 		_res: Response,
 		@Query payload: ListInsightsWorkflowQueryDto,
 	): Promise<InsightsByWorkflow> {
+		if (!this.licenseService.isInsightsDashboardEnabled()) {
+			throw new ForbiddenError('Insights dashboard is not enabled on the license');
+		}
+
 		return await this.insightsService.getInsightsByWorkflow({
 			maxAgeInDays: this.maxAgeInDaysFilteredInsights,
 			skip: payload.skip,
@@ -40,6 +49,10 @@ export class InsightsController {
 	@Get('/by-time')
 	@GlobalScope('insights:list')
 	async getInsightsByTime(): Promise<InsightsByTime[]> {
+		if (!this.licenseService.isInsightsDashboardEnabled()) {
+			throw new ForbiddenError('Insights dashboard is not enabled on the license');
+		}
+
 		return await this.insightsService.getInsightsByTime({
 			maxAgeInDays: this.maxAgeInDaysFilteredInsights,
 			periodUnit: 'day',
